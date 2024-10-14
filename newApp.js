e4e45d95fba981908c1566a9183ef097ceffbdd1
Oyelami <oyelamiabdulqudus@gmail.com>
2024-10-14 18:40:07 +0100

    added - objects,arrays and functio

--- newApp.js
+++ newApp.js
@@ -1,15 +1,90 @@
-// objects 
-const name = "jamal"
-const lastname = "adeleye"
-const age = 20 
-const ismarried = false 
-const address = "lagos"
-const skill1 = 'html'
-const skill2 = 'css'
-const skill3 = 'javaScript'
+// //==============================object
+// const firstname = "jamal";
+// const lastname = "adeleye";
+// const age = 20 ;
+// const ismarried = false; 
+// const address = "lagos";
+// const skill1 = 'html';
+// const skill2 = 'css';
+// const skill3 = 'javaScript';
 
-let person = {}
+// let person = {};
 
-//write a function that takes an objects and loogs it content
+// //write a function that takes an objects and loogs it content
+// console.log("This is an empty person object", person);
 
-console.log("This is an empty person object", person);
+// person = {
+//      firstname: "jamal",
+//      lastname: "adeleye",
+//      age: 20,
+//      ismarried: false, 
+//      address: "lagos",
+//      skill1: 'html',
+//      skill2: 'css',
+//      skill3: 'javaScript',
+
+// }
+// console.log(person); 
+// console.log(`The person's firstname: ${person.firstname}`); //string interpolation
+// console.log(`The person's lastname: ${person.lastname}`); //string interpolation
+// console.log('accessing object field with bracket notation', person['lastname']); //bracket notation
+
+//==================================array
+
+// person = [
+//     'jamal',
+//     'adeleya',
+//     20,
+//     false,
+//     'Lagos',
+//     'html',
+//     'css',
+//     'javascript'
+// ]
+// console.log(person);
+
+// person = {
+//     firstname: "jamal",
+//     lastname: "adeleye",
+//     age: 20,
+//     ismarried: false, 
+//     address: "lagos",
+//     skill1: 'html',
+//     skill2: 'css',
+//     skill3: 'javaScript',
+
+// }
+
+// function getObjecctKeyValue(key){
+//     console.log(`this is the value at key ${key}`, person[key]);
+// }
+
+// getObjecctKeyValue('firstname');
+// getObjecctKeyValue('lastname');
+// getObjecctKeyValue('age');
+
+// // converting objects into array 
+
+// const personAttributeArray = object.values(person)
+// const personAttributeArray2 = object.values(person)
+// console.log(personAttributeArray);
+// console.log(personAttributeArray2);
+
+
+//anonymous function
+
+// const func = function(){
+//     console.log('this is a function');
+    
+// }
+
+
+
+const arrayOfFood = ['rice', 'eba', 'ogbono', 'amala', 'egusi', 'wheat', 'abula',];
+for (const food of arrayOfFood) {
+    console.log("the current food menu is: ", food);    
+}
+
+
+
+
