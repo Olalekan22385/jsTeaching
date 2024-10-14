968bc0dd678341bda1f536c10db36108f3258af3
Oyelami <oyelamiabdulqudus@gmail.com>
2024-10-14 15:38:10 +0100

    anoda commit - added person details

--- newApp.js
+++ newApp.js
@@ -1,1 +1,15 @@
-console.log("I am learning js object, array and functions");
+// objects 
+const name = "jamal"
+const lastname = "adeleye"
+const age = 20 
+const ismarried = false 
+const address = "lagos"
+const skill1 = 'html'
+const skill2 = 'css'
+const skill3 = 'javaScript'
+
+let person = {}
+
+//write a function that takes an objects and loogs it content
+
+console.log("This is an empty person object", person);
